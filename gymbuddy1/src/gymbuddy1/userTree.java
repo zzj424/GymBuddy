35983270906b2f57a50285a5b6fa44adaf7a2189
Gaston <Gaston@10.250.235.89>
2018-04-08 15:10:37 -0700

    v1.1

--- gymbuddy1/src/gymbuddy1/userTree.java
+++ gymbuddy1/src/gymbuddy1/userTree.java
@@ -1,13 +1,33 @@
 package gymbuddy1;
 import java.util.*;
 public class userTree {
+	TreeSet<user> tree;
 	
-	
-	private class  user
+	private class  user implements Comparable
 	{
 		private String username;
 		private String password;
 		private String firstN;
+		private String lastN;
+		
+		private user(String u, String p, String f, String l)
+		{
+			username=u;
+			password=p;
+			firstN=f;
+			lastN=l;
+		}
+		
+		public int compareTo(Object u)
+		{
+			user x=(user)u;
+			if(this.username.compareTo(x.username)!=0)
+			{
+				return this.username.compareTo(x.username);
+			}
+			else
+			return this.password.compareTo(x.password);
+		}
 	}
 
 }
