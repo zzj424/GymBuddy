fcbe271e87d5f218afa875bf8f6e66c7de15cfeb
Gaston <Gaston@10.250.235.89>
2018-04-08 16:15:39 -0700

    v 1.4

---
+++ gymbuddy1/src/gymbuddy1/Test.java
@@ -0,0 +1,29 @@
+package gymbuddy1;
+
+import static org.junit.Assert.*;
+
+public class Test {
+ userTree tree1=new userTree();
+ 
+ 
+	@org.junit.Test
+	public void test() {
+		assertTrue("Not yet implemented");
+	}
+	
+	@org.junit.Test
+	public void test2() {
+		assertTrue("Not yet implemented");
+	}
+	
+	@org.junit.Test
+	public void test3() {
+		assertTrue("Not yet implemented");
+	}
+	
+	@org.junit.Test
+	public void test4() {
+		assertTrue("Not yet implemented");
+	}
+
+}
