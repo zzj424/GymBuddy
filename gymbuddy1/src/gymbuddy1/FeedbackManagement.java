ec0e1713f032ae93823bdd0b5c2333ddce17b18a
zzj424 <38477182+zzj424@users.noreply.github.com>
2018-05-03 10:45:02 -0700

    Update FeedbackManagement.java

--- gymbuddy1/src/gymbuddy1/FeedbackManagement.java
+++ gymbuddy1/src/gymbuddy1/FeedbackManagement.java
@@ -2,7 +2,7 @@ import java.sql.*;
 
 public class FeedbackManagement 
 {
-	public void addFeedback(int ID, String feedback)
+	public void addFeedback(String feedback)
 	{
 		try 
 		{
@@ -10,13 +10,29 @@ public class FeedbackManagement
 			Connection con = DriverManager.getConnection(
 				"jdbc:mysql://localhost:3306/gymbuddy", "qvbingo", "ognib646");
 			PreparedStatement oPrStmt = con
-				.prepareStatement("INSERT Feedback(ID,feedback) VALUES(?,?)");
-			oPrStmt.setInt(1,ID);
-			oPrStmt.setString(2, feedback);
+				.prepareStatement("SELECT * FROM counter where field = feedbacks* ");
+			
+			ResultSet resultFeedback=oPrStmt.executeQuery();
+			int id = resultFeedback.getInt("count");
 		
-			int rowsInserted= oPrStmt.executeUpdate();
+			String sql1="Insert INTO feedbacks(id, response) VALUES (?,?)";
+			PreparedStatement statement=con.prepareStatement(sql1);
+			
+			if(id==0){
+				id=1;
+			}
+			
+			statement.setInt(1,id);
+			statement.setString(2, feedback);
+		
+			
+			int rowsInserted= statement.executeUpdate(); 
 			if(rowsInserted > 0) {
-			System.out.println("A new feedback was collected successfully!");
+				System.out.println("A new feedback was collected successfully!");
+				id++;
+				String sql2="UPDATE counter SET count= ? WHERE field = feedback*";
+				PreparedStatement updateStmt = con.prepareStatement(sql2);
+				updateStmt.setInt(1, id);
 			}
 		
 		}catch (Exception e) {
